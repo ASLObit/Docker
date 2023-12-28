e2f6588c8130b439633c431add437d3def3fd299
ASLObit <93955852+ASLObit@users.noreply.github.com>
2023-12-27 22:29:41 -0500

    Update commands.sh

--- homeassistant/commands.sh
+++ homeassistant/commands.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+echo "Iniciando el script..."
+
 # Instalar curl y gdrive si no están instalados
 if ! command -v curl &> /dev/null; then
     echo "curl no está instalado. Instalando..."
@@ -12,8 +14,16 @@ if ! command -v gdrive &> /dev/null; then
     chmod +x /usr/local/bin/gdrive
 fi
 
+# Verificar si gdrive está instalado
+if command -v gdrive &> /dev/null; then
+    echo "gdrive está instalado."
+else
+    echo "Error: gdrive no se instaló correctamente."
+fi
+
 # Descargar el archivo utilizando curl
 curl -o /config/commands.sh https://raw.githubusercontent.com/ASLObit/Docker/main/homeassistant/commands.sh
 
 # Subir el archivo a Google Drive
 gdrive upload /config/backups/$(date +\%Y\%m\%d_\%H\%M\%S).tar --parent 1D0RP0i2ZWOc30jKMooqtgg9N2UdEIuR_
+
