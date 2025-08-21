8b9f059af49ecf418c8a43ca7fbc22725cfb9db0
ASLObit <93955852+ASLObit@users.noreply.github.com>
2025-08-21 14:33:43 -0500

    simit_checker

--- simit_checker/app/entrypoint.sh
+++ simit_checker/app/entrypoint.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-# Crear cronjob para correr cada día a las 9 AM
-echo "0 9 * * * root python /app/simit_checker.py >> /var/log/cron.log 2>&1" > /etc/cron.d/simit-cron
-
-chmod 0644 /etc/cron.d/simit-cron
-crontab /etc/cron.d/simit-cron
-
-# Iniciar cron y quedarse corriendo
+# Iniciar el servicio cron en foreground
 cron -f
