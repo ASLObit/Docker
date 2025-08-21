c3eec36424bbdb3581eb90d5aca73e7902a88bb4
ASLObit <aslo.bit@gmail.com>
2025-08-21 17:32:52 -0500

    simit_checker

--- simit_checker/app/simit_checker.py
+++ simit_checker/app/simit_checker.py
@@ -7,6 +7,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver as RemoteWebDriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import schedule
 
 PLACA = os.getenv("PLACA", "LKQ163")
 HA_URL = os.getenv("HA_URL")
