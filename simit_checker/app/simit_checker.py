947fe515a80546fdbca705cc35ae05dae26c4b5c
ASLObit <aslo.bit@gmail.com>
2025-08-21 16:33:51 -0500

    simit_checker

--- simit_checker/app/simit_checker.py
+++ simit_checker/app/simit_checker.py
@@ -18,17 +18,18 @@ def check_simit(placa):
     options.add_argument("--headless")
     options.add_argument("--disable-dev-shm-usage")
 
-    # Conectar a la instancia local de Chrome
-    service = Service('/usr/bin/chromedriver') # Ruta al ChromeDriver dentro del contenedor
+    service = Service('/usr/bin/chromedriver')
     driver = webdriver.Chrome(service=service, options=options)
     
     driver.get("https://www.simit.org.co/")
 
     try:
-        wait = WebDriverWait(driver, 20)
+        print("Esperando el elemento con ID 'placa'...")
+        wait = WebDriverWait(driver, 30) # Aumentar el tiempo de espera a 30 segundos
         element = wait.until(EC.presence_of_element_located((By.ID, "placa")))
         element.send_keys(placa)
         
+        print("Esperando el botón de búsqueda...")
         button = wait.until(EC.element_to_be_clickable((By.ID, "buscar")))
         button.click()
         
@@ -47,11 +48,16 @@ def check_simit(placa):
     return result
 
 def send_to_homeassistant(message):
+    print("Intentando enviar notificación a Home Assistant...")
     if HA_URL and HA_TOKEN:
         url = f"{HA_URL}/api/services/persistent_notification/create"
         headers = {"Authorization": f"Bearer {HA_TOKEN}", "Content-Type": "application/json"}
         payload = {"title": "Reporte SIMIT", "message": message}
-        requests.post(url, headers=headers, json=payload)
+        # Agregar `verify=False` para ignorar la verificación SSL
+        requests.post(url, headers=headers, json=payload, verify=False)
+        print("Notificación enviada.")
+    else:
+        print("HA_URL o HA_TOKEN no están configurados. No se enviará notificación.")
 
 def job():
     print(f"Ejecutando tarea a las {time.strftime('%H:%M:%S')}")
