b493c76f21d49bd5d76b7cc6706837e202338ae6
ASLObit <aslo.bit@gmail.com>
2025-09-26 16:42:54 -0500

    Remates Panamá

--- odoo_remates_panama/remates-panama-addons/invoice_stock_sync/__manifest__.py
+++
@@ -1,9 +0,0 @@
-{
-    "name": "Factura con Inventario",
-    "version": "18.0.1.0.0",
-    "author": "TuNombre",
-    "depends": ["account", "stock"],
-    "data": [],
-    "installable": True,
-    "application": False,
-}
