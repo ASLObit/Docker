2cda5e173af168c77e7b01eed9c08f65b6ba90d9
ASLObit <aslo.bit@gmail.com>
2025-10-02 18:19:37 -0500

    RP

--- rematespanama_stg/rematespanamastg-addons/invoice_auto_delivery/__manifest__.py
+++ rematespanama_stg/rematespanamastg-addons/invoice_auto_delivery/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Auto Delivery on Invoice Post",
     "summary": "Crea y valida la entrega al registrar facturas de cliente",
-    "version": "18.0.1.0.3",
+    "version": "18.0.1.0.4",
     "license": "LGPL-3",
     "author": "Tu Equipo",
     "depends": ["account", "stock", "stock_account"],
