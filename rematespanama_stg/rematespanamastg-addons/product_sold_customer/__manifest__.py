a25ae2fb2ff278f7953e0775dc81ca7bfc13c22e
ASLObit <aslo.bit@gmail.com>
2025-10-03 16:01:29 -0500

    RP

--- rematespanama_stg/rematespanamastg-addons/product_sold_customer/__manifest__.py
+++ rematespanama_stg/rematespanamastg-addons/product_sold_customer/__manifest__.py
@@ -11,3 +11,17 @@
     "application": False,
     "installable": True,
 }
+
+{
+    "name": "Product Sold on Customer Moves",
+    "version": "18.0.3.0.10",
+    "depends": [
+        "product",
+        "stock",
+        "sale",
+        "sale_stock",
+    ],
+    "data": [
+    ],
+    "installable": True,
+}
