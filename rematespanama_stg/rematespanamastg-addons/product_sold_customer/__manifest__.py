a68d33f4b6d28ac05bfecae7f063c4842410e80c
ASLObit <aslo.bit@gmail.com>
2025-10-03 16:01:40 -0500

    RP

--- rematespanama_stg/rematespanamastg-addons/product_sold_customer/__manifest__.py
+++ rematespanama_stg/rematespanamastg-addons/product_sold_customer/__manifest__.py
@@ -1,18 +1,4 @@
 {
-    "name": "Product Sold To Customer (no Sales)",
-    "summary": "Muestra unidades entregadas a cliente desde movimientos de stock",
-    "version": "18.0.3.0.9",
-    "license": "LGPL-3",
-    "author": "Tu Equipo",
-    "depends": ["stock"],
-    "data": [
-        "views/product_views.xml",
-    ],
-    "application": False,
-    "installable": True,
-}
-
-{
     "name": "Product Sold on Customer Moves",
     "version": "18.0.3.0.10",
     "depends": [
