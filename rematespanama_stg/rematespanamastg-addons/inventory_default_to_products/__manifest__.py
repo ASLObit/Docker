6716d5a145dfb6bd4867b57ce39d45814f6528cf
ASLObit <aslo.bit@gmail.com>
2025-10-04 21:52:12 -0500

    RP

---
+++ rematespanama_stg/rematespanamastg-addons/inventory_default_to_products/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Inventory – default to Products",
+    "version": "1.0.0",
+    "summary": "Al abrir Inventario va directo a Productos",
+    "category": "Inventory",
+    "depends": ["stock", "product"],
+    "data": ["data/menu_override.xml"],
+    "installable": True,
+    "application": False,
+}
