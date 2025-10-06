c6225d2f9fc9235ba3a2cf13a86573e00f8c2fb4
ASLObit <aslo.bit@gmail.com>
2025-10-05 19:23:49 -0500

    RP

--- rematespanama_stg/rematespanamastg-addons/currency_no_decimals/hooks.py
+++ rematespanama_stg/rematespanamastg-addons/currency_no_decimals/hooks.py
@@ -2,12 +2,11 @@
 from odoo import api, SUPERUSER_ID
 
 def post_init_hook(cr, registry):
-    """Al instalar: forzar redondeo entero para las monedas de todas las compañías."""
+    """Al instalar: forzar redondeo entero para las monedas de las compañías."""
     env = api.Environment(cr, SUPERUSER_ID, {})
     Company = env["res.company"].sudo()
     for company in Company.search([]):
         currency = company.currency_id
         if currency and currency.rounding != 1.0:
-            # En Odoo, decimal_places es computado a partir de 'rounding'.
-            # rounding=1.0  -> decimal_places=0 (sin decimales)
+            # rounding=1.0  => decimal_places=0 (sin decimales)
             currency.write({"rounding": 1.0})
