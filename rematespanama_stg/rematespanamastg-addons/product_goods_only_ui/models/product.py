2972c8343a966084055f424f10170899df21f836
ASLObit <aslo.bit@gmail.com>
2025-10-06 15:24:38 -0500

    RP

--- rematespanama_stg/rematespanamastg-addons/product_goods_only_ui/models/product.py
+++ rematespanama_stg/rematespanamastg-addons/product_goods_only_ui/models/product.py
@@ -1,68 +1,52 @@
-# -*- coding: utf-8 -*-
-from odoo import models, api
+# models/product.py
+from odoo import api, models
 
 class ProductTemplate(models.Model):
-    _inherit = "product.template"
-
-    @api.model
-    def default_get(self, fields_list):
-        vals = super().default_get(fields_list)
-        if "detailed_type" in self._fields:
-            vals["detailed_type"] = "product"
-        if "type" in self._fields:
-            vals["type"] = "product"
-        if "taxes_id" in self._fields:
-            vals["taxes_id"] = [(6, 0, [])]
-        if "supplier_taxes_id" in self._fields:
-            vals["supplier_taxes_id"] = [(6, 0, [])]
-        return vals
+    _inherit = 'product.template'
 
     @api.model_create_multi
     def create(self, vals_list):
         for vals in vals_list:
-            if "detailed_type" in self._fields:
-                vals["detailed_type"] = "product"
-            if "type" in self._fields:
-                vals["type"] = "product"
-            if "taxes_id" in self._fields:
-                vals["taxes_id"] = [(6, 0, [])]
-            if "supplier_taxes_id" in self._fields:
-                vals["supplier_taxes_id"] = [(6, 0, [])]
-        recs = super().create(vals_list)
-        for r in recs:
-            fix = {}
-            if "detailed_type" in r._fields and r.detailed_type != "product":
-                fix["detailed_type"] = "product"
-            if "type" in r._fields and getattr(r, "type", None) != "product":
-                fix["type"] = "product"
-            if "taxes_id" in r._fields and r.taxes_id:
-                fix["taxes_id"] = [(6, 0, [])]
-            if "supplier_taxes_id" in r._fields and r.supplier_taxes_id:
-                fix["supplier_taxes_id"] = [(6, 0, [])]
-            if fix:
-                super(ProductTemplate, r).write(fix)
-        return recs
+            # Forzar Bien solo usando detailed_type
+            vals.setdefault('detailed_type', 'product')
+            # dejar sin impuestos por defecto
+            vals.setdefault('taxes_id', [(6, 0, [])])
+            vals.setdefault('supplier_taxes_id', [(6, 0, [])])
+        return super().create(vals_list)
 
     def write(self, vals):
-        if "detailed_type" in vals:
-            vals["detailed_type"] = "product"
-        if "type" in vals:
-            vals["type"] = "product"
-        if "taxes_id" in vals:
-            vals["taxes_id"] = [(6, 0, [])]
-        if "supplier_taxes_id" in vals:
-            vals["supplier_taxes_id"] = [(6, 0, [])]
-        res = super().write(vals)
-        for r in self:
-            fix = {}
-            if "detailed_type" in r._fields and r.detailed_type != "product":
-                fix["detailed_type"] = "product"
-            if "type" in r._fields and getattr(r, "type", None) != "product":
-                fix["type"] = "product"
-            if "taxes_id" in r._fields and r.taxes_id:
-                fix["taxes_id"] = [(6, 0, [])]
-            if "supplier_taxes_id" in r._fields and r.supplier_taxes_id:
-                fix["supplier_taxes_id"] = [(6, 0, [])]
-            if fix:
-                super(ProductTemplate, r).write(fix)
-        return res
+        # Si no se está cambiando el tipo, asegurarlo como 'product'
+        if 'detailed_type' not in vals:
+            vals = dict(vals, detailed_type='product')
+        # Mantener sin impuestos si no se pasan explícitamente
+        if 'taxes_id' not in vals:
+            vals = dict(vals, taxes_id=[(6, 0, [])])
+        if 'supplier_taxes_id' not in vals:
+            vals = dict(vals, supplier_taxes_id=[(6, 0, [])])
+        return super().write(vals)
+# models/product.py
+from odoo import api, models
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            # Forzar Bien solo usando detailed_type
+            vals.setdefault('detailed_type', 'product')
+            # dejar sin impuestos por defecto
+            vals.setdefault('taxes_id', [(6, 0, [])])
+            vals.setdefault('supplier_taxes_id', [(6, 0, [])])
+        return super().create(vals_list)
+
+    def write(self, vals):
+        # Si no se está cambiando el tipo, asegurarlo como 'product'
+        if 'detailed_type' not in vals:
+            vals = dict(vals, detailed_type='product')
+        # Mantener sin impuestos si no se pasan explícitamente
+        if 'taxes_id' not in vals:
+            vals = dict(vals, taxes_id=[(6, 0, [])])
+        if 'supplier_taxes_id' not in vals:
+            vals = dict(vals, supplier_taxes_id=[(6, 0, [])])
+        return super().write(vals)
